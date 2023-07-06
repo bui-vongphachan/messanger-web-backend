3fe0736510a5511fff363cc91dd4dcc853c4fbb2
vongphachan~ <vongphachannukkalash@gmail.com>
2023-07-06 11:08:23 +0700

    used defined port from env file

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,7 @@ import { typeDefs } from "./type-defs";
     expressMiddleware(server)
   );
 
-  const PORT = 4000;
+  const PORT = process.env.PORT || 4000;
   // Now that our HTTP server is fully set up, we can listen to it.
   httpServer.listen(PORT, () => {
     console.log(`Server is now running on http://localhost:${PORT}/graphql`);
